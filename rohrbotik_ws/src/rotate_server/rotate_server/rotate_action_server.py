1fd36c2102e8849ba348bcba08e07cf17f29d6c0
johannes-wk <wk8228763@gmail.com>
2025-11-12 11:24:45 +0000

    Implement rotate_server initialization with action server and publisher/subscriber setup

--- rohrbotik_ws/src/rotate_server/rotate_server/rotate_action_server.py
+++ rohrbotik_ws/src/rotate_server/rotate_server/rotate_action_server.py
@@ -1,4 +1,5 @@
 import rclpy
+from interfaces.action import rotate 
 from rclpy.node import Node
 from rclpy.action import ActionServer
 from geometry_msgs.msg import Pose2D, Twist
@@ -8,7 +9,18 @@ import rotate_logic
 
 class rotate_server(Node):
     def __init__(self):
-        pass # to do !!!
+        super().__init__('simple_mover_server')
+        self._action_server = ActionServer ( self, rotate,'rotate',
+                                                self.execute_callback ,
+                                                goal_callback=self.goal_callback,        # optional: neue Ziele akzeptieren
+                                                cancel_callback=self.cancel_callback,    # optional: Abbrüche akzeptieren
+                                                handle_accepted_callback=self.handle_accepted_callback,# optional: wenn Ziel akzeptiert wird
+                                            ) 
+        
+        self.cmd_pub = self.create_publisher(Twist,'/cmd_vel', 10)
+        self.pose_sub = self.create_subscription(Pose2D,'/pose',self.pose_callback,10,callback_group=self.pose_cb_group)
+        self.get_logger().info(' SimpleMover Action Server gestartet.')
+      
     def goal_callback(self, goal_request):
         pass  # to do !!!
     def pose_callback(self,msg:Pose2D):
@@ -31,12 +43,12 @@ def main():
     current_pose = Pose2D(0.0, 0.0, 0.0)
    
 # event handel start 
-    if not kamera.seerohr == True :# interface :)
+    if not kamera.seerohr() == True :# interface :)
         if count == 0:
-            rotate_logic.Rotate.rotate_to_pipe(current_pose)
+            rotate_logic.RotateCL500.rotate_to_pipe(current_pose)
             count = 1
         else:
-            rotate_logic.Rotate.rotate_more(current_pose)
+            rotate_logic.RotateCL500.rotate_more(current_pose)
     else:
         # to do 
          # shut down implementiern 
--- rohrbotik_ws/src/rotate_server/rotate_server/rotate_logic.py
+++ rohrbotik_ws/src/rotate_server/rotate_server/rotate_logic.py
@@ -1,6 +1,6 @@
 import math
 
-class  Rotate :
+class  RotateCL500 :# patrice idee 'CL500'
     _omega = 0.63 # Drehgeschwindigkeit 
     _angle_threshold = 0.01  
     
@@ -13,20 +13,20 @@ class  Rotate :
 
         angle_diff = self.normalize_angle(angle_to_target - current_pose[2])
 
-        if abs(angle_diff) < Rotate._angle_threshold:
+        if abs(angle_diff) < RotateCL500._angle_threshold:
             return True
         
-        return [0., Rotate._omega]
+        return [0., RotateCL500._omega]
         
 
     def rotate_more(self,current_pose):
         new_angel=current_pose[2] + 79* math.pi / 180 # 79 ist eine primzahl das heißt in 5 umdrehungen sind wir einmal um den kreiß und weiter sso stellen wir sicher das wir mit der kamera auf jeden fall das rohr erkenn 
         angle_diff = self.normalize_angle(new_angel - current_pose[2])
 
-        if abs(angle_diff) < Rotate._angle_threshold:
+        if abs(angle_diff) < RotateCL500._angle_threshold:
             return True
         
-        return [0., Rotate._omega]
+        return [0., RotateCL500._omega]
 
     def normalize_angle(angle):
         
