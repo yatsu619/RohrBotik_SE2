6de77607d68265af3f007c96bc6405338fbf7c29
johannes-wk <wk8228763@gmail.com>
2025-11-13 16:01:49 +0000

    Refactor rotate_server to improve pose handling and integrate rotate_logic for command execution

--- rohrbotik_ws/src/rotate_server/rotate_server/rotate_action_server.py
+++ rohrbotik_ws/src/rotate_server/rotate_server/rotate_action_server.py
@@ -3,34 +3,58 @@ from interfaces.action import rotate
 from rclpy.node import Node
 from rclpy.action import ActionServer
 from geometry_msgs.msg import Pose2D, Twist
-#from # action  import  # action 
+from datagate import cam
 import time
 import rotate_logic
 
 class rotate_server(Node):
     def __init__(self):
-        super().__init__('simple_mover_server')
-        self._action_server = ActionServer ( self, rotate,'rotate',
-                                                self.execute_callback ,
+        super().__init__('rotate_server')
+        self.current_pose = Pose2D(0.0, 0.0, 0.0)
+        self._action_server = ActionServer ( self, rotate,'rotate',         
+                                                self.execute_callback,          
                                                 goal_callback=self.goal_callback,        # optional: neue Ziele akzeptieren
                                                 cancel_callback=self.cancel_callback,    # optional: Abbrüche akzeptieren
                                                 handle_accepted_callback=self.handle_accepted_callback,# optional: wenn Ziel akzeptiert wird
                                             ) 
         
         self.cmd_pub = self.create_publisher(Twist,'/cmd_vel', 10)
-        self.pose_sub = self.create_subscription(Pose2D,'/pose',self.pose_callback,10,callback_group=self.pose_cb_group)
-        self.get_logger().info(' SimpleMover Action Server gestartet.')
+        self.pose_sub = self.create_subscription(Pose2D,'/pose',self.pose_callback,10,)
+        self.get_logger().info(' Rotate Action Server gestartet.')
       
     def goal_callback(self, goal_request):
-        pass  # to do !!!
+        self.get_logger().info(' Neues Ziel empfangen!')
+        return rclpy.GoalResponse.ACCEPT  
+
     def pose_callback(self,msg:Pose2D):
-        pass # to do !!!
+        self.current_pose.x=msg.x
+        self.current_pose.y=msg.y
+        self.current_pose.theta=msg.theta
+        self.get_logger().info(f'Pose empfangen: x={msg.x}, y={msg.y}, theta={msg.theta}')
+
     def cancel_callback(self, goal_handle):
-        pass # to do !!!
+        if not cam.seerohr() == True :# interface :) @Patrice115
+            self.get_logger().info('Abbruchanfrage empfangen!')
+            return rclpy.CancelResponse.ACCEPT
+        else:
+            return rclpy.CancelResponse.REJECT
+    
     def handle_accepted_callback(self, goal_handle):
         pass # to do !!!
     def execute_callback(self, goal_handle):
-        pass  # to do !!!
+        count = 0
+
+        cmd_actuell = Twist()
+    
+    # event handel start 
+        
+        if count == 0:
+            cmd_actuell =  rotate_logic.RotateCL500.rotate_to_pipe(self.current_pose) 
+            count = 1
+        else:
+            cmd_actuell =rotate_logic.RotateCL500.rotate_more(self.current_pose)
+        
+    #  wiederholung !! 
 
 
 
@@ -39,21 +63,20 @@ class rotate_server(Node):
 
 
 def main():
-    count = 0
-    current_pose = Pose2D(0.0, 0.0, 0.0)
-    cmd_actuell = Twist()
-   
-# event handel start 
-    if not kamera.seerohr() == True :# interface :)
-        if count == 0:
-            cmd_actuell =  rotate_logic.RotateCL500.rotate_to_pipe(current_pose)
-            count = 1
-        else:
-            cmd_actuell =rotate_logic.RotateCL500.rotate_more(current_pose)
-    else:
-        # to do 
-         # shut down implementiern 
-         pass
- #  wiederholung !! 
+
+    try:
+        pass # to do !!!
+    except KeyboardInterrupt:
+        node.get_logger().info(' Server wird beendet...')
+    finally:
+        executor.shutdown()
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+
+
+
+
 if __name__ == '__main__':
     main()
