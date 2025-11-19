74af6ec6fa724d7cd3faf1cec1fc45b1aa261ed0
johannes-wk <wk8228763@gmail.com>
2025-11-19 13:24:33 +0000

    Refactor rotate_to_pipe method to remove unnecessary target angle initialization

--- rohrbotik_ws/src/rotate_server/rotate_server/rotate_logic.py
+++ rohrbotik_ws/src/rotate_server/rotate_server/rotate_logic.py
@@ -3,16 +3,13 @@ import math
 class  RotateCL500 :# patrice idee 'CL500'
     _omega = 0.63 # Drehgeschwindigkeit 
     _angle_threshold = 0.01  
-    @staticmethod
-    def __init__():
-        ''' Setzt die anfangsbedingung auf Null '''
-        _target_angle = None
-        
+   
+
     @staticmethod
     def rotate_to_pipe(current_pose):
         ''' Dreht sich um 180 Grad Gedacht um nach der Zweiten Fahrt schneller zu sein '''
-        if RotateCL500._target_angle is None:
-            angle_to_target = 180 * math.pi / 180 # winkel in Radianten 
+        angle_to_target=current_pose[2]
+        angle_to_target += 180 * math.pi / 180 # winkel in Radianten 
 
         angle_diff = RotateCL500.normalize_angle(angle_to_target - current_pose[2])
 
@@ -27,6 +24,7 @@ class  RotateCL500 :# patrice idee 'CL500'
              79 Grad da es eine primzahl ist und so sichergestellt ist das die gleichen spots nicht doppelt aufgerufen werden 
              Außerdem werden so realativ schnell alle 4 Quartale abgedeckt --> Schnelles rohr finden + sichergestellt das das Rohr zu 100% gefunden wird  '''
         new_angel=current_pose[2] + 79* math.pi / 180 # 79 ist eine primzahl das heißt in 5 umdrehungen sind wir einmal um den kreiß und weiter sso stellen wir sicher das wir mit der kamera auf jeden fall das rohr erkenn 
+        #new_angel=RotateCL500.normalize_angle(new_angel)
         angle_diff = RotateCL500.normalize_angle(new_angel - current_pose[2])
 
         if abs(angle_diff) < RotateCL500._angle_threshold:
