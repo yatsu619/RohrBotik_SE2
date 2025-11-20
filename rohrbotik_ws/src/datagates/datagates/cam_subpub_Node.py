72cf110fb09e2333eaeb79200a7ae14fc8899dc5
Patrice <patrice.wenzig@stud.hs-kempten.de>
2025-11-20 05:56:51 +0100

    PFusch- doppelpublish-winkel-bool

--- rohrbotik_ws/src/datagates/datagates/cam_subpub_Node.py
+++ rohrbotik_ws/src/datagates/datagates/cam_subpub_Node.py
@@ -1,4 +1,5 @@
 from sensor_msgs.msg import Image
+from std_msgs.msg import Float32, Bool
 from cv_bridge import CvBridge
 import numpy as np
 import cv2 
@@ -25,11 +26,16 @@ class Camera_data(Node):
             self.frame_callback,
             10
         )
-#        self.pub_cam_data = self.create_publisher(
-#            MarkerInfo,
-#            'marker_info', 
-#            10
-#        )
+        self.pub_winkel = self.create_publisher(
+            Float32,
+            'winkel_info', 
+            10
+        )
+        self.pub_marker_seh = self.create_publisher(
+            Bool,
+            'found_marker',
+            10
+        )
 
         self.get_logger().info("SupPubNode gestartet")
 
@@ -44,13 +50,31 @@ class Camera_data(Node):
         else:
             self.get_logger().info("Da knallts an der Kreuzung gewaltig")
 
+# @@@@@@@@@@@@@@@@   AB hier quick&Dirty lösung für nur zwei publisher   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+        if not self.vp.find_ArUco(self.gray_frame):
+            self.get_logger().info("Es wurde kein Marker gefunden, oder zu viele...")
+        else:
+            marker_found = self.vp.marker_gefunden
+            marker_winkel = self.vp.marker_winkel
+
+            msg_winkel = Float32()
+            msg_winkel.data = float(marker_winkel)
+            self.pub_winkel.publish(msg_winkel)
+        
+            msg_found = Bool()
+            msg_found.data = marker_found
+            self.pub_marker_seh.publish(msg_found)
+
+
+""" <<<<<<<<<<<<<<<<<<<<<<<<<<<< Kommt wieder, wenn wir einen sauberen CustomMessager gebaut haben >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #Visionprocessor arbeitet
 
         if not self.vp.find_ArUco(self.gray_frame):   #TODO: Sollten wir einen kurzen time.Sleep(0.05) einbauen, damit der Visionprozessor das bild verarbeiten und die variablen neu schreiben kann, bevor wir sie HIER eine zeile später abfragen?
             self.get_logger().info("Es wurde kein Marker gefunden, oder zu viele...")
-#            msg_out = MarkerInfo()
-#            msg_out.detected = False
-#            self.pub_cam_data.publish(msg_out)
+            msg_out = MarkerInfo()
+            msg_out.detected = False
+            self.pub_cam_data.publish(msg_out)
             return False
         
         marker_id = self.vp.marker_id
@@ -59,16 +83,23 @@ class Camera_data(Node):
         marker_found = self.vp.marker_gefunden
 
      
-#        msg_out = MarkerInfo() #TODO custommessage von Yatheesh
-#        msg_out.detected = marker_gefunden
-#        msg_out.angle = marker_winkel
-#        msg_out.distance = marker_distanz
-#        msg_out.marker_id = marker_id
+        msg_out = MarkerInfo() #TODO custommessage von Yatheesh
+        msg_out.detected = marker_gefunden
+        msg_out.angle = marker_winkel
+        msg_out.distance = marker_distanz
+        msg_out.marker_id = marker_id
         
-#        self.pub_cam_data.publish(msg_out)
+        self.pub_cam_data.publish(msg_out)
 
         self.get_logger().info(f'ArUco gefunden: {marker_found} ID: {marker_id}, distanz: {marker_distanz: .2f}, Winkel: {marker_winkel: .2f}°')
 
+        <<<<Für oben dann... >>>>>
+#        self.pub_cam_data = self.create_publisher(
+#            MarkerInfo,
+#            'marker_info', 
+#            10
+#        )
+"""
 
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
