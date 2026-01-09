a849daec8f2d5bc71e7c815d991593ef6703ad43
johannes-wk <wk8228763@gmail.com>
2026-01-09 15:29:03 +0100

    Enhance docstrings for PID control methods
    Updated method docstrings to provide detailed explanations and descriptions for parameters and return values.

--- rohrbotik_ws/src/move_server/move_server/move_logic.py
+++ rohrbotik_ws/src/move_server/move_server/move_logic.py
@@ -3,21 +3,40 @@ import time
 
 
 class PID: 
+    """   Einfache Steuerung für Roboterbewegungen mit Winkel- und Abstandskorrektur.
+  Zielwinkel für die Regelung (0°),Verstärkung für Winkelkorrektur (1.1),Verstärkung für Abstandsregelung (1.3). """
     _sollwinkel =0
     _gain = 1.1
     _gain_folgen =1.3
 
     @staticmethod
     def zur_mitte_regeln(aktueller_winkel,soll_geschwindigkeit_linear ):
-        """ Regler zur mitte hin braucht den winkel um den er regelen muss sowie die voreingestellte geschwindigkeit 
-         returnt die lineare Geschwindigkeit sowie die winkelgeschwingkeit  """
+         """
+        Berechnet die Steuerung, um den Roboter auf den Sollwinkel (Mitte) auszurichten.
+        Args:aktueller_winkel (float): Aktuell gemessener Winkel [Grad].
+            soll_geschwindigkeit_linear (float): Vorgabe für die lineare Geschwindigkeit.
+        Returns:tuple: (lineare Geschwindigkeit, Winkelgeschwindigkeit in rad/s)
+        Beschreibung:
+            Die Methode berechnet die Winkelgeschwindigkeit proportional zur Abweichung
+            vom Sollwinkel. Die lineare Geschwindigkeit bleibt konstant.
+        """
         angular_velocity = (((aktueller_winkel - PID._sollwinkel)*PID._gain)* math.pi / 180)     
         return soll_geschwindigkeit_linear, angular_velocity
         
     @staticmethod
     def abstand_und_winkel_regeln(aktueller_winkel,soll_geschwindigkeit_linear,distanz_nach_vorne_gemessen, soll_abstand):
-        """ Regler der den Abstand regelt aufgrund der distanz zum Marker und einer soll_geschwindigkeit dazu sort der das der bot gerade zur mitte Föhrt  """
-        """ gemessener Winkel , Richtgeschwindigkeit , Abstand zum Marker (gemessen)"""
+          """
+        Berechnet lineare und Winkelgeschwindigkeit basierend auf Abstand und Winkel.
+        Args:aktueller_winkel (float): Aktuell gemessener Winkel [Grad].
+            soll_geschwindigkeit_linear (float): Vorgabe für die lineare Geschwindigkeit.
+            distanz_nach_vorne_gemessen (float): Gemessener Abstand zum Marker/Objekt.
+            soll_abstand (float): Gewünschter Soll-Abstand zum Objekt.
+        Returns: tuple: (lineare Geschwindigkeit, Winkelgeschwindigkeit in rad/s)
+        Beschreibung:
+            Die Methode passt die lineare Geschwindigkeit proportional zur Abweichung 
+            vom Soll-Abstand an und korrigiert gleichzeitig den Winkel zur Sollrichtung.
+            Die lineare Geschwindigkeit wird auf [-0.05, soll_geschwindigkeit_linear] begrenzt.
+        """
 
         angular_velocity = (((aktueller_winkel - PID._sollwinkel)*PID._gain)* math.pi / 180) 
 
